ad8a62252c7e4e506d2b54d6c469e70aecd80acf
Orhun Ecemis <105704418+oecemis1@users.noreply.github.com>
2024-09-14 15:40:42 +0300

    Update install_nix.sh

--- install_nix.sh
+++ install_nix.sh
@@ -1,11 +1,23 @@
 #!/usr/bin/env bash
 
 nix-env -iA nixpkgs.neovim
+nix-env -iA nixpkgs.vim
 nix-env -iA nixpkgs.helix
 nix-env -iA nixpkgs.yazi
-nix-env -iA nixpkgs.fzf
-nix-env -iA nixpkgs.btop
 
+nix-env -iA nixpkgs.fzf
 if ! grep -q "eval \"\$(fzf --bash)\"" "$HOME/.bashrc"; then
     echo 'eval "$(fzf --bash)"' >> "$HOME/.bashrc"
 fi
+
+nix-env -iA nixpkgs.btop
+
+# helix language servers
+nix-env -iA nixpkgs.nodePackages_latest.bash-language-server
+nix-env -iA nixpkgs.nodePackages.diagnostic-languageserver
+nix-env -iA nixpkgs.pyright
+nix-env -iA nixpkgs.nixd
+nix-env -iA nixpkgs.verible
+nix-env -iA nixpkgs.verilator
+nix-env -iA nixpkgs.clang-tools
+nix-env -iA nixpkgs.lldb
