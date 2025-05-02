04e5279077b455d0da79f10d8ecd5f738ef62f25
oecemis1 <o.ecemis@etu.edu.tr>
2025-05-02 06:53:23 +0300

    Add bluetooth and audio drivers

--- nixos/odyssey/configuration.nix
+++ nixos/odyssey/configuration.nix
@@ -70,6 +70,37 @@ in
   # Workaround for GNOME autologin
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+    settings = {
+      Policy = {
+        AutoEnable = "false";
+      };     
+    };
+  };
+
+  services.blueman.enable = true;
   
   # Basic system packages
   environment.systemPackages = with pkgs; [
