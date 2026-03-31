e6b95e83737a1469b1a6f6bee8ee54247e4da6fc
oecemis1 <orhun.ecemis@gmail.com>
2026-04-01 01:35:39 +0300

    refactor: update laptop power settings

--- hosts/odyssey/power-management.nix
+++ hosts/odyssey/power-management.nix
@@ -15,23 +15,24 @@ _: {
     enable = true;
     settings = {
       CPU_DRIVER_OPMODE_ON_AC = "active";
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "power";
 
       CPU_DRIVER_OPMODE_ON_BAT = "active";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
       CPU_MIN_PERF_ON_AC = 0;
-      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MAX_PERF_ON_AC = 70;
       CPU_MIN_PERF_ON_BAT = 1;
-      CPU_MAX_PERF_ON_BAT = 60;
+      CPU_MAX_PERF_ON_BAT = 50;
 
       CPU_BOOST_ON_BAT = 1;
       CPU_BOOST_ON_AC = 1;
       CPU_HWP_DYN_BOOST_ON_BAT = 1;
+      CPU_HWP_DYN_BOOST_ON_AC = 1;
 
-      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_AC = "quiet";
       PLATFORM_PROFILE_ON_BAT = "quiet";
 
       # Battery care settings
