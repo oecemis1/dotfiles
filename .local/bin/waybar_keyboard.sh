38f8c3e5e7284b6d2333c8ddc28e49e08d1aa342
oecemis1 <orhun.ecemis@gmail.com>
2026-05-24 16:24:00 +0200

    fix(waybar keyboard): use json format for reliability

--- .local/bin/waybar_keyboard.sh
+++ .local/bin/waybar_keyboard.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-layout=$(hyprctl devices | grep -A 3 'asus-keyboard' | grep -m 1 "active keymap: " | awk -F': ' '{print $2}')
+layout=$(hyprctl devices -j | jq -r '.keyboards[0].active_keymap')
 echo "$layout"
